dc302a3aafda0d083471ff5071f16921f8eb9a50
Erik Storås Sommer <erikssommer@gmail.com>
2023-01-28 14:36:04 +0100

    Added general game methods

--- mcts/node.py
+++ mcts/node.py
@@ -15,3 +15,20 @@ class Node:
         self.visits += 1
         self.rewards += reward
     
+    def reward(self):
+        """
+        Return the reward of the state represented by the node
+        """
+        return self.state.reward()
+
+    def is_game_over(self):
+        """
+        Return True if the game represented by the state is over, False otherwise
+        """
+        return self.state.is_game_over()
+    
+    def apply_action(self, action):
+        """
+        Apply an action to the state represented by the node
+        """
+        self.state.apply_action(action)
