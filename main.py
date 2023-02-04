f75eb64a486632b5efecca236d74a4721d6826a0
Erik Storås Sommer <erikssommer@gmail.com>
2023-02-04 17:47:20 +0100

    Working on playing a game against mcts

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from stats.rbuf import RBUF
 from nim.nim import NimGame
+from mcts.mcts import MCTS
 
 def main():
     # Read environment variables
@@ -16,15 +17,25 @@ def main():
     # TODO: Randomly initialize parameters (weights and biases) of ANET
 
     # For g_a in number actual games
-    for _ in range(nr_of_games):
-        # Initialize the actual game board (Ba) to an empty board.
+    for i in range(nr_of_games):
+        # Initialize the actual game board (B_a) to an empty board.
         game = NimGame(nr_of_piles)
 
-        # While Ba not in a final state:
+        # TODO: s_init ← starting board state
+
+        # Initialize the Monte Carlo Tree (MCT) to a single root, which represents s_init
+        tree = MCTS(game)
+
+        # While B_a not in a final state:
         while not game.is_over():
+            # Initialize Monte Carlo game board (Bmc) to same state as root.
+            tree.root = game.get_state() # TODO: method needed
+            node = tree.search(game.player)
+            game.play(node.state)
             game.print_piles()
             action = game.get_action()
             game.play(action)
+            game.print_piles()
         print(f"Player {str(game.get_winner())} wins!")
 
 
