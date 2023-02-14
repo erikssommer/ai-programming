d68e737570939794fc417527179b61baae0068ee
nicolmol <56063595+nicolaimol@users.noreply.github.com>
2023-02-14 10:33:28 +0100

    remake nim

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+from stats.rbuf import RBUF
+from nim.nim import NimGame
+from mcts.mcts import MCTS
+
+def main():
+    # Read environment variables
+    rbuf_size = int(os.environ['RBUF_SIZE'])
+    nr_of_games = int(os.environ['NR_OF_GAMES'])
+    nr_of_piles = int(os.environ['NR_OF_PILES'])
+    # TODO: i_s = save interval for ANET (the actor network) parameters
+
+    # Clear Replay Buffer (RBUF)
+    rbuf = RBUF(rbuf_size)
+
+    # TODO: Randomly initialize parameters (weights and biases) of ANET
+
+    # For g_a in number actual games
+    for i in range(nr_of_games):
+        # Initialize the actual game board (B_a) to an empty board.
+        game = NimGame(nr_of_piles)
+
+        # TODO: s_init ← starting board state
+
+        # Initialize the Monte Carlo Tree (MCT) to a single root, which represents s_init
+        tree = MCTS(game)
+
+        # While B_a not in a final state:
+        while not game.is_game_over():
+            # Initialize Monte Carlo game board (Bmc) to same state as root.
+            tree.root = game.get_state() # TODO: method needed
+            node = tree.search(game.player)
+            game.play(node.state)
+            game.print_piles()
+            action = game.get_player_action()
+            game.play(action)
+            game.print_piles()
+        print(f"Player {str(game.get_winner())} wins!")
+
+
+
+if __name__ == '__main__':
+    # Load environment variables
+    load_dotenv()
+    main()
--- nim/nim.py
+++ nim/nim.py
@@ -1,18 +1,20 @@
 import random
 import tkinter as tk
+from time import sleep
+
+from copy import deepcopy
 
 
 class NimGame:
-    def __init__(self, n, solo=False):
+    def __init__(self, n):
         """
         :param n: number of piles
-        :param solo: if the game is played solo or two players
         """
         self.n = n
-        self.solo = solo
-        self.piles = list(range(1, n + 1))
         self.player = True
         self.moves = []
+
+        self.game_state = [[1 for _ in range(i)] for i in range(1, n + 1)]
 
         self.root = tk.Tk()
         self.root.title("nim")
@@ -20,102 +22,133 @@ class NimGame:
         self.root.resizable(False, False)
         self.labels = []
 
-    def apply_action(self, action):
+    def apply_action(self, input_action):
         """
-        :param action: the action to be applied to the current state (pile, stones)
+        :param input_action: the action to be applied to the current state (pile, stones)
         :return: if the game is over
         """
-        self.piles[action[0]] -= action[1]
-        self.moves.append((self.player, action))
-        if self.is_game_over():
+
+        if not self.validate_action(input_action):
             return False
-        if self.solo:
-            self.make_bot_move()
+
+        start = deepcopy(self.game_state)
+
+        for i in range(input_action[1]):
+            # set input_action[1] last 1 to 0
+            for j in range(len(self.game_state[input_action[0]])):
+                if self.game_state[input_action[0]][-j - 1] == 1:
+                    self.game_state[input_action[0]][-j - 1] = 0
+                    break
+
+        end = self.game_state
+
+        if start == end:
+            print("Start", start)
+            print("End", end)
+            print("Action", input_action)
+            print("Game state", self.game_state)
+            raise Exception("Invalid action")
+
+        self.moves.append((self.player, input_action))
 
         self.player = not self.player
 
         return True
 
-    def make_bot_move(self):
-        """
-        Make a random move for the bot
-        """
-        print("Bot is thinking...")
-        actions = self.get_children()
-        # random move
-        action = actions[random.randint(0, len(actions) - 1)]
-        self.piles[action[0]] -= action[1]
-        self.moves.append(("bot", action))
+    def get_state(self):
+        return self.game_state
 
     def get_children(self):
         """
         :return: the list of all the possible actions for the current state
         """
+
         actions = []
+        for index, i in enumerate(self.game_state):
+            for j in range(index + 1):
+                actions.append((index, j + 1))
+
+        """actions = []
         for pile in range(self.n):
             for stones in range(1, self.piles[pile] + 1):
-                actions.append((pile, stones))
+                actions.append((pile, stones))"""
         return actions
 
-    def validate_action(self, action):
+    def validate_action(self, action_to_validate):
         """
-        :param action: the action to be validated
+        :param action_to_validate: the action to be validated
         :return: if the action is valid for the current state
         """
-        if self.piles[action[0]] >= action[1]:
-            return True
-        else:
+        if len(list(filter(lambda x: (x == 1), self.game_state[action_to_validate[0]]))) < action_to_validate[1]:
+            #print("Values", len(list(filter(lambda x: (x == 1), self.game_state[action_to_validate[0]]))), action_to_validate[1])
             return False
+        return True
 
-    def get_action(self):
+    def get_player_action(self, ):
         """
-        :return: the action chosen by the player
-        """
-        print(f"Player {self.player}, Choose a pile and the number of stones to remove from it.")
+                :return: the action chosen by the player
+                """
+
+        print(f"Player {1 if self.player else 2}, Choose a pile and the number of stones to remove from it.")
         pile = input(f"Pile: ")
         stones = input(f"Stones: ")
         if self.validate_action((int(pile), int(stones))):
             return int(pile), int(stones)
         else:
             print("Invalid action!")
-            return self.get_action()
+            return self.get_player_action()
 
     def is_game_over(self):
         """
-        :return: if the game is over
+        :return: if the game is over, all values in the game state are 0
         """
-        return all(element == 0 for element in self.piles)
+        return all(element == 0 for element in flatten(self.game_state))
 
     def get_winner(self):
         """
         :return: the winner of the game
         """
-        return not self.player
-
-    def get_piles(self):
-        """
-        :return: the piles of the game (pile, number of stones)
-        """
-        return self.piles
+        return self.player
 
     def print_piles(self):
         """
-        Print the piles of the game
+        Print the piles of the game state
+        GUI used is tkinter
         """
         for label in self.labels:
             label.destroy()
         self.labels = []
 
-        for index, value in enumerate(self.piles):
+        if not self.is_game_over():
+
             label = tk.Label(
                 self.root,
-                text=f"Pile: {index}, stones: {' '.join([f'O' for _ in range(value)])} \n",
+                text=f"Player: {self.get_player()}",
                 anchor='w'
             )
-
             label.pack(fill='both')
-
             self.labels.append(label)
+
+            for index, value in enumerate(self.game_state):
+                label = tk.Label(
+                    self.root,
+                    text=f"Pile: {index}, stones: {' '.join([f'O' for _ in range(len(list(filter(lambda x: (x == 1), value))))])} \n",
+                    anchor='w'
+                )
+
+                label.pack(fill='both')
+
+                self.labels.append(label)
+
+        else:
+            label = tk.Label(
+                self.root,
+                text=f"Player {self.get_player()} wins!",
+                anchor='w'
+            )
+            label.pack(fill='both')
+            self.labels.append(label)
+        self.root.update()
 
     def get_player(self):
         """
@@ -134,21 +167,33 @@ class NimGame:
                f"Player: {str(self.player)}"
 
 
-def main():
-    solo = input("Do you want to play solo? (y/n): ")
-    if solo == "y":
-        n = input("How many piles do you want to play with? ")
-        game = NimGame(int(n), True)
-    else:
-        n = input("How many piles do you want to play with? ")
-        game = NimGame(int(n))
+def flatten(input_list: list) -> list:
+    """
+    Flatten a list of lists, only works for 2d lists
+    :param input_list:
+    :return: makes a list of lists into a list
+    """
+    return [item for sublist in input_list for item in sublist]
+
+
+def demo():
+    """
+    Demo of the game
+    Two random players play the game
+    :return:
+    """
+
+    game = NimGame(4)
+
     while not game.is_game_over():
         game.print_piles()
-        action = game.get_action()
-        game.apply_action(action)
-
-    print(f"Player {str(game.get_winner())} wins!")
-
-
-if __name__ == "__main__":
-    main()
+        actions = game.get_children()
+        action = actions[random.randint(0, len(actions) - 1)]
+        made = game.apply_action(action)
+        if made:
+            print(flatten(game.game_state))
+            print(actions)
+            sleep(2)
+    game.print_piles()
+    sleep(2)
+    print(f"You {'won' if game.get_winner() else 'lost'}!")
