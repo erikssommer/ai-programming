f44ed6c9057ec7399e31a1864532628b02573d3c
nicolmol <56063595+nicolaimol@users.noreply.github.com>
2023-03-09 12:35:30 +0100

    uses config for ui in topp

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def train_models():
 
 def play_topp():
     # Initialize the Tournament of Progressive Policies (TOPP)
-    topp = TOPP(config.nr_of_anets, config.nr_of_topp_games, ui=True)
+    topp = TOPP(config.nr_of_anets, config.nr_of_topp_games, ui=config.topp_ui)
 
     # Add the agents to the tournament
     topp.add_agents()
