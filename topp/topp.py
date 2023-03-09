ad293b0abcb32e1bfea903d515625aaa91cebaab
Erik Storås Sommer <erikssommer@gmail.com>
2023-03-09 15:57:50 +0100

    Plotting results

--- topp/topp.py
+++ topp/topp.py
@@ -6,7 +6,9 @@ from ui.hex import HexUI
 from utility.read_config import config
 import os
 import random
-import pygame
+import matplotlib.pyplot as plt
+from IPython import display
+from IPython.utils import io
 
 # The Tournament of Progressive Policies (TOPP)
 
@@ -23,7 +25,7 @@ class TOPP:
         for file in os.listdir(policy_path):
             if file.endswith(".pt"):
                 self.agents.append(Agent(policy_path, file))
-        
+
         # Terminate if there are no agents
         if len(self.agents) == 0:
             print("No agents found, exiting...")
@@ -67,11 +69,11 @@ class TOPP:
 
                         if self.ui:
                             ui.draw_board()
-                            sleep(0.5)
+                            sleep(0.1)
 
                     # Record the result of the game
                     winner = game.get_winner()
-                    
+
                     # Update the agents win/loss/draw
                     if starting_agent == i and winner == 1:
                         self.agents[i].add_win()
@@ -89,14 +91,51 @@ class TOPP:
                         self.agents[i].add_draw()
                         self.agents[j].add_draw()
 
+                    # Update the plot
+                    if self.ui:
+                        self.update_plot()
+
                     # Swap the starting player
                     if starting_agent == i:
                         starting_agent = j
                     else:
                         starting_agent = i
 
+    def update_plot(self):
+        with io.capture_output() as captured:
+            display.clear_output(wait=True)
+            display.display(plt.gcf())
+            plt.clf()
+            # x is agent name
+            x = [agent.name for agent in self.agents]
+            # y is number of wins
+            y = [agent.win for agent in self.agents]
+            # specify colors for each bar
+            colors = ['red', 'green', 'blue', 'purple', 'orange',
+                      'yellow', 'pink', 'brown', 'black', 'grey']
+            plt.bar(x, y, color=colors)
+            # Set with of display
+            plt.title('Topp Statistics')
+            plt.xlabel('Agent wins')
+            plt.ylabel('Number of Games')
+            plt.show(block=False)
+
     def get_results(self):
         agents_result = sorted(self.agents, key=lambda x: x.win, reverse=True)
 
         for agent in agents_result:
-            print(f"Agent {agent.name} won {agent.win} times, lost {agent.loss} times and drew {agent.draw} times")
+            print(
+                f"Agent {agent.name} won {agent.win} times, lost {agent.loss} times and drew {agent.draw} times")
+
+        x = [agent.name for agent in self.agents]
+        # y is number of wins
+        y = [agent.win for agent in self.agents]
+        # specify colors for each bar
+        colors = ['red', 'green', 'blue', 'purple', 'orange',
+                  'yellow', 'pink', 'brown', 'black', 'grey']
+        plt.bar(x, y, color=colors)
+        # Set with of display
+        plt.title('Topp Statistics')
+        plt.xlabel('Agent wins')
+        plt.ylabel('Number of Games')
+        plt.show(block=True)
