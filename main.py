c93599ee76a46839f34998dca3eb4319cd6d56c1
Erik Storås Sommer <erikssommer@gmail.com>
2023-03-24 11:27:52 +0100

    Testing mcts, something is off

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from utility.read_config import config
 from topp.topp import TOPP
 from utility.timer import Timer
 from rl.reinforcement_learning import RL
+from tqdm import tqdm
+from mcts.mcts import MCTS
+from nn.on_policy import OnPolicy
+from managers.state_manager import StateManager
+import random
+
 
 # Main file for training and playing the Tournament of Progressive Policies (TOPP)
 
@@ -44,6 +50,39 @@ def delete_models():
         os.remove(os.path.join('./nn_models', file))
 
 
+def test_mcts():
+    victories = 0
+
+    for _ in tqdm(range(100)):
+        state_manager: StateManager = StateManager.create_state_manager()
+        game_state = state_manager.get_game_state()
+
+        ann = OnPolicy(states=config.board_size ** 2 + 1,
+                 actions=config.board_size ** 2)
+
+        tree = MCTS(game_state, 1, 1, 1000, 1, dp_nn=ann)
+
+        while not state_manager.is_game_over():
+
+            if state_manager.get_player() == 1:
+                best_move_node, distribution = tree.search(state_manager.get_player())
+
+                state_manager.perform_action(best_move_node.state)
+
+            else:
+                actions = state_manager.get_legal_actions()
+                action = random.choice(actions)
+
+                state_manager.apply_action_self(action)
+
+        if state_manager.get_winner() == 1:
+            victories += 1
+        
+        print(state_manager.get_winner())
+
+    print(victories)
+
+
 if __name__ == "__main__":
     setup()
     if config.train:
@@ -54,3 +93,5 @@ if __name__ == "__main__":
         timer.end_timer()
     if config.topp:
         play_topp()
+    if config.test_mcts:
+        test_mcts()
